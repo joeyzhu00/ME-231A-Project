d25d42e5d542fa07de711c9e2ecdd1fb67885f96
Joey Zhu <joeyzhu00@gmail.com>
2019-12-10 14:13:39 -0800

    added dynamics to optimization

--- bike_model.m
+++ bike_model.m
@@ -1,4 +1,4 @@
-function zp = bike_model(z, u, sampleTime, vehicleParams)
+function zp = bike_model(z, u, sampleTime, VehicleParams)
 % Function representing the kinematic bicycle model
 %
 % INPUT:
@@ -26,9 +26,29 @@ function zp = bike_model(z, u, sampleTime, vehicleParams)
 
 zp = sdpvar(4,1);
 
-zp(1) = z(1)+sampleTime*z(3)*cos(z(4)+u(2));
-zp(2) = z(2)+sampleTime*z(3)*sin(z(4)+u(2));
-zp(3) = z(3)+sampleTime*u(1);
-zp(4) = z(4)+sampleTime*z(3)*sin(u(2))/vehicleParams.lr;
+% global frame kinematics/dynamics
+yawRate = z(3)*sin(u(2))/VehicleParams.lr;
+xVelocity = z(3)*cos(z(4)+u(2));
+yVelocity = z(3)*sin(z(4)+u(2));
+
+% slip angle calculations
+% front tires
+alphaF = z(4) + VehicleParams.lf*yawRate/z(3) - u(2);
+% rear tires
+alphaR = z(4) - VehicleParams.lr*yawRate/z(3);
+
+% tire force calculation
+Fyf = -VehicleParams.Cf*alphaF;
+Fyr = -VehicleParams.Cr*alphaR;
+
+xAccel = yawRate*yVelocity + u(1);
+yAccel = -yawRate*xVelocity + (2/VehicleParams.mass)*(Fyf*cos(u(2)) + Fyr);
+yawAccel = (2/VehicleParams.Izz)*(VehicleParams.lf*Fyf - VehicleParams.lr*Fyr);
+
+zp(1) = z(1) + sampleTime*xVelocity + (sampleTime^2)*xAccel;
+zp(2) = z(2) + sampleTime*yVelocity + (sampleTime^2)*yAccel;
+zp(3) = z(3) + sampleTime*u(1);
+zp(4) = z(4) + sampleTime*yawRate + (sampleTime^2)*yawAccel;
+
 
 end
