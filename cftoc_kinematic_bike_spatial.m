6f7be60464f04ccd1b22eabd537e16672b202c23
Tuan Tu <xtuantux@gmail.com>
2019-12-12 12:00:13 -0800

    cleaned up and revised comments to clarify

--- cftoc_kinematic_bike_spatial.m
+++ cftoc_kinematic_bike_spatial.m
@@ -1,50 +1,12 @@
 function [feas, zOpt, uOpt, JOpt] = cftoc_kinematic_bike_spatial(N, z0, sampleTime, VehicleParams, IneqConstraints, ObstacleParams, rhoS, avoidTune, trackTune)
 
-% %%% DELETE THESE LATER. FOR TESTING ONLY %%%
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% N = 10;
-% z0 = [0; 0; 30; 0]; 
-% sampleTime = 0.1; % [sec]
-% VehicleParams.lf = 4.65/2; % [m]
-% VehicleParams.lr = 4.65/2; % [m]
-% VehicleParams.trackWidth = 1.78; % [m]
-% rhoS = 10000; % 10km radius of curvature path. Approximate a straight road
-% 
-% 
-% %Constraints 
-% %upper state constraints
-% IneqConstraints.zMax = [10000; 5; z0(3)+1; 45*pi/180];
-% % upper state constraints
-% IneqConstraints.zMin = [0; -5; z0(3)-1; -45*pi/180];
-% % lower input constraints
-% IneqConstraints.uMin = [-0.5; -30*pi/180];
-% % upper input constraints
-% IneqConstraints.uMax = [0.5; 30*pi/180];
-% % limit on difference b/t current and previous steering input
-% IneqConstraints.betaRange = 0.05; % [rad]
-% % limit on longitudinal acceleration
-% IneqConstraints.longAccelRange = 0.06; % [m/s^2]
-% 
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % Obstacle Bounds/Centroid %
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % coordinates of the obstacle centroid (x,y)
-% ObstacleParams(1).centroids = [20; .5]; % [m]
-% % obstacle vertex boundaries (assuming everything is rectangular)
-% %xMin, xMax, yMin, yMax]
-% ObstacleParams(1).bounds = [-1.5, 1.5, -0.5, 0.5]; % [m]
-% % ObstacleParams(2).centroids = [40; .5]; % [m]
-% % ObstacleParams(2).bounds = [-1, 1, -1, 1]; % [m]
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%{ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Function to solve the constrained finite time optimal control problem
-% with a kinematic bicycle model to track a global path. Using spatial
+% with a kinematic bicycle model to track straight line path using spatial
 % state instead of time-states. 
 % 
 % ***NOTE***: Straight Line Only. To make it work for any path, will need
-% more math so the s_coordinate is a vector that is the path
+% more math for using curvilinear coordinate system
 %
 % INPUT:
 %       N - double
@@ -73,6 +35,19 @@ function [feas, zOpt, uOpt, JOpt] = cftoc_kinematic_bike_spatial(N, z0, sampleTi
 %           Contains: centroids - position of centroids in global frame
 %                     bounds - boundaries of the obstacle surrounding the
 %                              centroid in the local obstacle frame
+%       rhoS - double
+%           Radius of curviture of the road. Keep this number HIGH (ex:
+%           10000) so this will work. Maybe change in future to make this
+%           workable on a constant curve of radius rhoS. Can also make rhoS
+%           change at each function call if radius of curvature is
+%           changing.
+%       avoidTune - double
+%           Tuning knob to determine how closely the vehicle dodge the
+%           obstacle. Used in customer function when the vehicle is passing
+%           obstacles.
+%       trackTune - double
+%           Tuning knon to adjust how aggressively the vehicle try to track
+%           the centerline. Used at each step and at terminal step cost. 
 %
 % OUTPUTS:
 %      feas - bool
@@ -86,20 +61,19 @@ function [feas, zOpt, uOpt, JOpt] = cftoc_kinematic_bike_spatial(N, z0, sampleTi
 %      
 %      JOpt - double (1,N)
 %          Cost
-%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Convert time-dependent state to spatial states
-
-
 sampleDist = sampleTime*z0(3); % initialize the be the same at k=0
 
 ePsi0 = z0(4); %epsi0 = heading- psiRoad, psiRoad=0 at initial
 ey0 = z0(2); % eY0 = VehGlobalY-RoadGlobalY. Setting the road at the x-axis to Global Frame, if not, need to subtract.
-vx0 = z0(3); % speed in body-frame
+vx0 = z0(3); % speed in body-frame. meter per meter. 
 zSpatial0 = [ePsi0; ey0; vx0];
 
-% number of inputs [Veh heading in road-spatial-frame; 
-%                   Veh y position in road-spatial-fram]
+% number of states [Veh heading in road-spatial-frame; 
+%                   Veh y position in road-spatial-frame;
+%                   Veh speed. meter per meter]
 nz = length(zSpatial0);
 
 % Number of inputs [longitudinal accel; steering angle]. 
@@ -111,12 +85,15 @@ u = sdpvar(nu, N);
 % initial condition constraint
 constraints = z(:,1) == zSpatial0;
 % initialize the cost
-inputCostTune = 1;
-% tune this to add padding around obstacles, higher = MORE padding
-% too small will make MPC not feasible! 
 
-%Terminal cost
-cost = trackTune*z(2,N+1)^2 + inputCostTune*u(2,N)^2;
+% Consider tuning this to smooth or inputs, or just use u(k+1)-u(k)
+% constraints instead.
+inputCostTune = 1;
+
+% Terminal cost
+% Currently only penalizing y-position tracking.
+cost = trackTune*z(2,N+1)^2; 
+
 %Terminal Constraints
 constraints = [constraints,...
     -pi/6<=z(1,N+1)<=pi/6]; % soft constraints on final heading
@@ -126,7 +103,7 @@ for k=1:N;
 constraints = [constraints,...
     z(1,k+1) == z(1,k)+sampleDist*(((rhoS-z(2,k))*sin(u(2,k)))/(rhoS*z(3,k)*cos(z(1,k)))-1/rhoS),...
     z(2,k+1) == z(2,k)+sampleDist*(sin(z(1,k))*(rhoS-z(2,k)))/(rhoS*cos(z(1,k))),...
-    z(3,k+1) == z(3,k)+0*sampleDist*u(1,k)]; % NO BRAKING FOR NOW
+    z(3,k+1) == z(3,k)+0*sampleDist*u(1,k)]; % NO BRAKING FOR NOW. Remove zero to add braking/accel
 % State Constraints
 constraints = [constraints,...
     IneqConstraints.zMin(4)<=z(1,k+1)<=IneqConstraints.zMax(4),... %Heading
@@ -135,9 +112,10 @@ constraints = [constraints,...
 % Input Constraints
 constraints = [constraints, IneqConstraints.uMin(1)<=u(1,k)<=IneqConstraints.uMax(1),...
     IneqConstraints.uMin(2)<=u(2,k)<=IneqConstraints.uMax(2)];
-
 % Cost
+% Currently only penalizing y-position tracking and steering input.
 cost = cost + trackTune*z(2,k)^2 + inputCostTune*u(2,k)^2;
+
 end
 
 % Avoidance heuristic and calculations. 
@@ -145,14 +123,18 @@ end
 % obstacles s-bounds, any-bounds from given initial condition z0
 avoid = avoidHeuristic(ObstacleParams, z0, sampleDist);
 
-
-
+% State Constraints to avoid obstacle.
+% Obstacle x coordinates have been converted to spatial s
+% Obstacle y coordinate have been converted to ey-distance from road
+% centerline 
 for j=1:size(ObstacleParams,2); %loop through all obstacles 
-    for k=avoid.s(j,1):avoid.s(j,2) %loop through state s of each obstacle
-        if and(avoid.mode(j,1)==1,k<=N+1)
+    for k=avoid.s(j,1):avoid.s(j,2) %loop through space s of each obstacle
+        if and(avoid.mode(j,1)==1,k<=N+1) %avoid left if obstacle is right of Veh heading
+            % vehicle y position greater than obstacle bound y max position
             constraints = [constraints, z(2,k)>=(avoid.y(j,2)+VehicleParams.trackWidth/2)];
             cost = cost - avoidTune*(z(2,k)-(avoid.y(j,2)+VehicleParams.trackWidth/2))^2;
-        elseif and(avoid.mode(j,1)==2,k<=N+1)
+        elseif and(avoid.mode(j,1)==2,k<=N+1) %avoid right if obstacle is left of Veh heading
+            % vehicle y position less than obstacle bound y min position
             constraints = [constraints, z(2,k)<=(avoid.y(j,1)-VehicleParams.trackWidth/2)];
             cost = cost - avoidTune*(z(2,k)-(avoid.y(j,2)-VehicleParams.trackWidth/2))^2;
         end
@@ -162,7 +144,7 @@ end
 options = sdpsettings('verbose', 0, 'solver', 'ipopt');
 diagnostics = optimize(constraints, cost, options);
 
-
+% Convert spatial s to time t. See reference literature for more discussion
 interpTime=zeros(1,N+1);
 if (diagnostics.problem == 0)
     feas = true;
@@ -181,44 +163,13 @@ else
     return
 end
 
-% Convert Spatial States to Time-Dependent State
+% Convert Spatial States to Time-Dependent State using linear interpolation
 zOpt(1,:) = z0(1)+interp1(interpTime,linspace(0,N*sampleDist,N+1),linspace(0,N*sampleTime,N+1)); %x global
 zOpt(2,:) = interp1(interpTime,zOptSpatial(2,:),linspace(0,N*sampleTime,N+1)); %y global
 zOpt(3,:) = interp1(interpTime,zOptSpatial(3,:),linspace(0,N*sampleTime,N+1)); %speed 
 zOpt(4,:) = interp1(interpTime,zOptSpatial(1,:),linspace(0,N*sampleTime,N+1)); %heading
-
+% Convert Spatial input to Time-Dependent input
 uOpt(1,:) = interp1(interpTime(1:end-1),uOptSpatial(1,:),linspace(0,N*sampleTime,N)); %Steering Angle 
 uOpt(2,:) = interp1(interpTime(1:end-1),uOptSpatial(2,:),linspace(0,N*sampleTime,N)); %Speed Change
 
-
-%returns interpolated values of a 1-D function at specific query points using linear interpolation. Vector x contains the sample points, and v contains the corresponding values, v(x). Vector xq contains the coordinates of the query points.
-
-% figure('name', 'position')
-% % plot the obstacles
-% for k = 1:length(ObstacleParams)
-%     rectangle('Position', [ObstacleParams(k).centroids(1)+ObstacleParams(k).bounds(1), ObstacleParams(k).centroids(2)+ObstacleParams(k).bounds(3), ...
-%                           -ObstacleParams(k).bounds(1)+ObstacleParams(k).bounds(2), -ObstacleParams(k).bounds(3)+ObstacleParams(k).bounds(4)]);
-%     hold on
-%     plotLegend{k+2} = sprintf('Obstacle #%d', k);
-%     plot(ObstacleParams(k).centroids(1), ObstacleParams(k).centroids(2), 'o');
-%     hold on
-% end
-% plot(sampleDist*(0:N), zOptSpatial(2,:))
-% hold on
-% plot(zOpt(1,:), zOpt(2,:),'o')
-% hold off
-
-% figure('name', 'heading')
-% subplot(2,1,1)
-% plot(sampleDist*(0:N), zOptSpatial(1,:))
-% legend('heading vs spatial position')
-% subplot(2,1,2)
-% plot(linspace(0,N*sampleTime,N+1), zOpt(4,:))
-% legend('heading vs time')
-
-
-
-
-
-
-
+end
