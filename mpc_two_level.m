d4f8270422d11396ee8bd3dfc72b52374966fdd6
liueric101 <liu493@usc.edu>
2019-12-15 18:34:31 -0800

    File rename and update

--- mpc_two_level.m
+++ mpc_two_level.m
@@ -1,4 +1,4 @@
-%{
+function [feas, zOpt, uOpt, JOpt, pursuitPoints] = mpc_kinematic_bike(M, N, z0, vehiclePath, sampleTime, VehicleParams, stopCondition, ObstacleParams)
 % Function to facilitate MPC for the kinematic bicycle to track a global
 % path. The lower state constraints are currently static to force the
 % vehicle to keep moving along the path. An obstacle avoidance cost is
@@ -24,6 +24,10 @@
 %          Contains: lf - distance from CM to front wheel [m]
 %                    lr - distance from CM to rear wheel [m]
 %                    trackWidth - Axle width [m]
+%                    Cf - Front tire cornering coefficient [N/rad]
+%                    Cr - Rear tire cornering coefficient [N/rad]
+%                    Izz - Yaw Inertia [kg*m^2]
+%                    mass - Vehicle Mass [kg]
 %
 %       stopCondition - double
 %           Threshold to stop the MPC when the XY-positions are within the
@@ -47,44 +51,17 @@
 %      JOpt - double (1,M)
 %          Cost from each iteration
 %
-%}
-clear all
-
-% THIS ONLY WORK FOR STRAIGHT LINE PATH!!! %
-% Need to decide workflow from here. Pass the z(:,N+1) at each step OR
-% wait until MPC complete and pass zOpt(:,M+1)?
-
-N=5;
-M=30;
-z0 = [0; 0; 30; 0]; 
-sampleTime = 0.1; % [sec]
-% vehicle wheelbase
-VehicleParams.lf = 4.65/2; % [m]
-VehicleParams.lr = 4.65/2; % [m]
-% vehicle track width
-VehicleParams.trackWidth = 1.78; % [m]
-%Path - ALMOST STRAIGHT LINE 
-rhoS = 10000; % 10km radius of curvature path. Approximate a straight road
-
-ObstacleParams(1).centroids = [30; 0.5]; % [m]
-% obstacle vertex boundaries (assuming everything is rectangular)
-% [xMin, xMax, yMin, yMax]
-ObstacleParams(1).bounds = [-2.5, 2.5, -0.5, 0.5]; % [m]
-
-ObstacleParams(2).centroids = [55; 0];
-ObstacleParams(2).bounds = [-1.5, 1.5, -0.5, 0.5]; % [m]
-% INTERESTINGLY this fail at [60,0], car try to return to center too fast
-% and becomes infeasible (heading straight at 2nd obstacle because
-% N-horizon is too short and don't see it until too late!
+%
+% NOTE: Currently only allows the vehicle to track a global path,
+% vehiclePath
 
 % number of states
 nz = length(z0);
 % number of inputs [longitudinal accel; steering angle]
 nu = 2;
 
-IneqConstraints.zMax = [10000; 5; z0(3)+1; 60*pi/180];
 % upper state constraints
-IneqConstraints.zMin = [0; -5; z0(3)-1; -60*pi/180];
+IneqConstraints.zMax = [vehiclePath(1,end); 6; 40; 2*pi];
 % lower input constraints
 IneqConstraints.uMin = [-0.5; -30*pi/180];
 % upper input constraints
@@ -94,69 +71,69 @@ IneqConstraints.betaRange = 0.05; % [rad]
 % limit on longitudinal acceleration
 IneqConstraints.longAccelRange = 0.06; % [m/s^2]
 
-zOpt = zeros(nz, M+1);
-uOpt = zeros(nu, M);
-JOpt = zeros(1,M);
-feas = false([1, M]);
-
-% initial conditions
-zOpt(:,1) = z0;
-uOpt(:,1) = [0;0];
-
 % tune this to add padding around obstacles, higher = MORE padding
 % too small will make MPC not feasible easier! We can't prove where this
 % algorithm is persistently feasible :(. It is not in certain conditions. 
 % THERE IS POSSIBILITY TO HEAD STRAIGHT FOR OBSTACLE IF N IS NOT LONG
 % ENOUGH TO "SEE" IT IN THE CFTOC. DISCUSS THIS IN PAPER WRITE UP!
-avoidTune = 1;
+avoidTune = 3.0;
 
 % Tune how closely the vehicle track the path. Higher is aggressive
 % tracking
-trackTune = avoidTune/5;
+trackTune = 1.5;
 
-figure('name', 'position')
+
+%Path - ALMOST STRAIGHT LINE 
+rhoS = 10000; % 10km radius of curvature path. Approximate a straight road
+
+zOpt = zeros(nz, M+1);
+uOpt = zeros(nu, M);
+JOpt = zeros(1,M);
+feas = false([1, M]);
+planFeas = false([1, M]);
+pursuitPoints = zeros(nz, N+1, M);
+
+% initial conditions
+zOpt(:,1) = z0;
+uOpt(:,1) = [0;0];
 for i = 1:M
     fprintf('Working on MPC Iteration #%d \n', i);
-    tic
-    % lower state constraint is dynamic, 
-    % (not needed) IneqConstraints.zMin = [zOpt(1,i); -3; 0; -2*pi];
+    % find the point to pursue
+%     pursuitPoint = find_pursuit_point(zOpt(:,i), uOpt, VehicleParams, vehiclePath, N, sampleTime);
+    IneqConstraints.zMin = [zOpt(1,i); -6; 0; -2*pi];
+    
+    % do the high-level MPC to figure out the path
+    [planFeas(i), zSpatial, ~, ~] = cftoc_kinematic_bike_spatial(N, zOpt(:,i), sampleTime, VehicleParams, IneqConstraints, ObstacleParams, rhoS, avoidTune, trackTune);
+
+    if ~planFeas(i)
+        disp('High Level infeasible region reached!');
+        return;
+    end
+    pursuitPoints(:,:,i) = zSpatial;
+
+    %display(zSpatial)
+    
+    % lower state constraint is dynamic
     % solve the cftoc problem for a kinematic bicycle model and run in "open-loop"
-    [feas(i), z, u, cost] = cftoc_kinematic_bike_spatial(N, zOpt(:,i), sampleTime, VehicleParams, IneqConstraints, ObstacleParams, rhoS, avoidTune, trackTune);
-    toc
+    [feas(i), z, u, cost] = cftoc_kinematic_bike(N, zOpt(:,i), sampleTime, VehicleParams, IneqConstraints, pursuitPoints(:,:,i));
     
     if ~feas(i)
         disp('Infeasible region reached!');
         return
-    end
-    
-    %plot open loops
-    plot(z(1,:),z(2,:))
-    hold on
+    end    
     % closed loop predictions
-    zOpt(:,i+1) = z(:,2);
     uOpt(:,i) = [u(1,1); u(2,1)];
     JOpt(:,i) = cost;
-%    pause()
+%     zOpt(:,i+1) = z(:,2);
+    % update state dynamics with the dynamic bicycle model
+    zOpt(:,i+1) = dynamic_bike_model(zOpt(:,i), uOpt(:,i), sampleTime, VehicleParams);
+    
+    
     % exit the for loop if the vehicle positions are within the stopCondition threshold
-%     if (abs(zOpt(1,i+1)-vehiclePath(1,end)) <= stopCondition) && (abs(zOpt(2,i+1)-vehiclePath(2,end)) <= stopCondition)
-%         disp('Reached the end of the path');
-%         break;
-%     end
-
+    if (abs(zOpt(1,i+1)-vehiclePath(1,end)) <= stopCondition) && (abs(zOpt(2,i+1)-vehiclePath(2,end)) <= stopCondition)
+        disp('Reached the end of the path');
+        break;
+    end
 end
 
-% plot the obstacles
-for k = 1:length(ObstacleParams)
-    rectangle('Position', [ObstacleParams(k).centroids(1)+ObstacleParams(k).bounds(1), ObstacleParams(k).centroids(2)+ObstacleParams(k).bounds(3), ...
-        -ObstacleParams(k).bounds(1)+ObstacleParams(k).bounds(2), -ObstacleParams(k).bounds(3)+ObstacleParams(k).bounds(4)]);
-    hold on
-    plotLegend{k+2} = sprintf('Obstacle #%d', k);
-    plot(ObstacleParams(k).centroids(1), ObstacleParams(k).centroids(2), 'o');
-    hold on
 end
-
-%plot closed loop
-plot(zOpt(1,:),zOpt(2,:),'o')
-
-zOpt
-uOpt
